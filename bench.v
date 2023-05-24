e75adf909e7c57415897414aaf492fe16b9e8b4b
momoya-memma <133881171+momoya-memma@users.noreply.github.com>
2023-05-25 00:02:36 +0900

    Update bench.v

--- bench.v
+++ bench.v
@@ -1,59 +1,63 @@
 `timescale 1ns/1ps
 
-module tb_Prpg10;/*このモジュール名がsim実行結果のファイル名になる。*/
-  reg clk, tx ;/*テストベンチ内で使用するレジスタを宣言する*/
-  wire [3:0] ingicater;
-  wire  [3:0] ja;
-  wire  [3:0] jb;
+module sim_result;/*このモジュール名がsim実行結果のファイル名になる。*/
+  reg clk ;/*テストベンチ内で使用するレジスタを宣言する*/
+  reg ck_rst;
+  reg rx;
+  wire [3:0] ja;
+  wire [3:0] jb;
 
-  reg [6:0] segment;
-  wire digit;
+  parameter UART_STEP = 104160;/*period 1000ns=1usec*/
+  parameter CYC = 10;/*10ns = 100MHz*/
 
-  assign ja[0] = segment[0];
-  assign ja[1] = segment[1];
-  assign ja[2] = segment[2];
-  assign ja[3] = segment[3];
-  assign jb[0] = segment[4];
-  assign jb[1] = segment[5];
-  assign jb[2] = segment[6];
-  assign jb[3] = digit;
+  always #(CYC/2) clk=~clk;
 
-
-  parameter STEP = 104160; //104usec
-  parameter MILLI = 1000000;//1milli sec
-  parameter CYC = 10;/*period 10ns=1u/100=100MHz*/
-  always #(CYC/2) clk = ~clk;/*CYC/2が経過するたびにclkを反転*/
-
-  seven_seg_disp disp ( /*sim対象のモジュールをdutという名前でインスタンス化*/
-    .CLK100MHZ (clk),/*モジュールのポートCLK100MHZに（）の中身の値を対応づける*/
-    .uart_rx_of_pmod (tx),
-    .led (ingicater),
-    .ja (ja),
-    .jb (jb)
+  top_module test_module ( /*sim対象のモジュールをdutという名前でインスタンス化*/
+    .CLK100MHZ (clk)/*モジュールのポートCLK100MHZに（）の中身の値を対応づける*/
+    , .ck_rst (ck_rst)
+    , .rx (rx)
+    , .ja (ja)
+    , .jb (jb)
   );
 
   initial begin
-    $dumpfile("tb_Prpg10.vcd"); // vcd file name
-    $dumpvars(0,tb_Prpg10);     // dump targetは「全部」
+    $dumpfile("sim_result.vcd"); // vcd file name
+    $dumpvars(0,sim_result);     // dump targetは「全部」
 
     // Initilai value
-    #(CYC* 0)   clk=0; segment=7'b0000000; tx=1;
+    #(CYC* 0)   clk=0;ck_rst=0;rx=1;
 
-    #(STEP* 5)  
     // Set seed
-    #(STEP)   tx=0;   //tx start
-    #(STEP)   tx=1;   //ascii 5 1of8
-    #(STEP)   tx=0;   //ascii 5 2of8
-    #(STEP)   tx=1;   //ascii 5 3of8
-    #(STEP)   tx=0;   //ascii 5 4of8
-    #(STEP)   tx=1;   //ascii 5 5of8
-    #(STEP)   tx=1;   //ascii 5 6of8
-    #(STEP)   tx=0;   //ascii 5 7of8
-    #(STEP)   tx=0;   //ascii 5 8of8
-    #(STEP)   tx=1;   //
+    #(CYC*100)   ck_rst=1'b1;   //
+
+    #(CYC*10)        rx=1'b0;   //start bit
+    #(UART_STEP*1)   rx=1'b1;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b1;   //
+    #(UART_STEP*1)   rx=1'b1;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b1;   //end bit
+
+    #(UART_STEP*5)         rx=1'b0;   //start bit
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b1;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b1;   //
+    #(UART_STEP*1)   rx=1'b1;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b0;   //
+    #(UART_STEP*1)   rx=1'b1;   //end bit
+
+
+
+
 
     // Stop simulation
-    #(MILLI*20)   $finish;
+    #(UART_STEP*3)   $finish;
   end
   
 endmodule
